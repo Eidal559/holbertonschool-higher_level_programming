50cde5a794d531ba41168284bc05f39fafa907e3
Ladie juarbe <139393459+Eidal559@users.noreply.github.com>
2024-01-29 12:59:05 -0400

    task-0

--- pyhon-more_data_structures/0-square_matrix_simple.py
+++ pyhon-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[x * x for x in row] for row in matrix]
+    result_matrix = [[0 for _ in row] for row in matrix]
 
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        result_matrix[i][j] = matrix[i][j] ** 2
-        
-        return result_matrix
-    
-    input_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    
-    result = square_matrix_simple(input_matrix)
-    print(result)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            result_matrix[i][j] = matrix[i][j] ** 2
+
+    return result_matrix
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
