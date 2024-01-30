b960a6c6404d2be2bf5fffe11ef743e20c3b2e91
Eidal559 <eidaljuarbe@gmail.com>
2024-01-30 11:18:51 -0400

    all-tasks

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,11 @@ def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
         try:
-            print(f"{my_list[i]}", end="")
-            total += 1
+            pass
         except IndexError:
             break
+        else:
+            print("{}".format(my_list[i]), end="")
+            total += 1
     print()
-    return(total)
+    return total
