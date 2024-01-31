9ce8821479f7bce2eb8f3479a6d61550f42411ec
Eidal559 <eidaljuarbe@gmail.com>
2024-01-31 10:04:23 -0400

    task-0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""This module creates a class Square"""
-class square:
+"""This module defines an empty class Square."""
 
-    def __init__(self, size=0):
-        """Initializes the data."""
-        self.__size = size
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+class Square:
+    """Represents a square."""
+    pass
