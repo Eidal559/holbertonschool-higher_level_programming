e264b8875030fc96a73d51631a133b8fec1253df
Ladie juarbe <139393459+Eidal559@users.noreply.github.com>
2024-02-05 13:07:51 -0400

    all-tasks

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
 class Rectangle:
-    """Empty class that defines a rectangle"""
-    def __init__(self):
-        pass
+    """An empty class that represents a rectangle"""
+    pass
