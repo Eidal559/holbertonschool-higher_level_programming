a0023c6d90a11132710ac61c0fc47426f9a1b789
Ladie juarbe <139393459+Eidal559@users.noreply.github.com>
2024-02-06 09:46:39 -0400

    all-tasks

--- python-more_classes/2-main.py
+++ python-more_classes/2-main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+Rectangle = __import__('2-rectangle').Rectangle
 
-from 2-rectangle import Rectangle
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
-if __name__ == "__main__":
-    my_rectangle = Rectangle(2, 4)
-    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+print("--")
 
-    print("--")
-
-    my_rectangle.width = 10
-    my_rectangle.height = 3
-    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+my_rectangle.width = 10
+my_rectangle.height = 3
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
