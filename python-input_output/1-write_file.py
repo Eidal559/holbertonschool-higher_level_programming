cc0698965a736a1b7f78fed802884ccc61235345
Eidal559 <eidaljuarbe@gmail.com>
2024-02-08 12:59:57 -0400

    all-tasks

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
-    with open(filename, "w", encoding="utf-8") as file:
-        num_chars_written = file.write(text)
-    return num_chars_written
+"""This module defines a file-writing function."""
 
-# Example usage:
-text = "Hello, world!"
-filename = "output.txt"
-num_chars_written = write_file(filename, text)
-print(f"Number of characters written to {filename}: {num_chars_written}")
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
